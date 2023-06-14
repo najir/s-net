a6b3aa3d5f08f60d917a5c1be14d4e376975f57e
Najir <irrperks@gmail.com>
2023-06-14 14:51:44 -0700

    Started outlining classes and needed structures

--- src/lexer.h
+++ src/lexer.h
@@ -1,11 +1,43 @@
 #ifndef LEXER_H
 #define LEXER_H
+#include <iostream>
+
+struct Tokens{
 
 
 
 
 
+    std::string type_to_string() const{
 
+
+    }
+};
+
+class InputSource{
+protected:
+
+public:
+
+};
+
+class StandardInputSource : InputSource {
+
+
+
+};
+
+class FileInputSource : InputSource {
+
+
+};
+
+class Lexor {
+private:
+
+public:
+
+};
 
 
 #endif
