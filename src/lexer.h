add4f14b2e02ac63077eb44f11bb4265299821c1
Najir <irrperks@gmail.com>
2023-06-14 23:42:04 -0700

    Few more function declerations, started setting up list of tokens

--- src/lexer.h
+++ src/lexer.h
@@ -2,20 +2,37 @@
 #define LEXER_H
 #include <iostream>
 #include <fstream>
+#include <vector>
 
-struct Tokens{
+struct Tokens{ //Expect to be moved to it's own header file
+    enum class Type{
+        eof         = 1,
+        eol         = 2,
+        indentation = 3,
+        identifier  = 4,
+        def         = 5,
+        symbol      = 6,
+        _class      = 7,
+        _import     = 8,
+        _return     = 9,
+        _if         = 10,
+        _else       = 11,
+        _elif       = 12,
+        _and        = 13,
+        _or         = 14,
+        _while      = 15,
+        _int        = 16,
+        _string     = 17,
+        _operator   = 18,
+        UNKNOWN     = 19,
+        lComment    = 20,
+        bComment    = 21,
+        _scope      = 22
 
-
-
-
-
-    std::string type_to_string() const{
-
-
-    }
+    };
 };
 
-struct Errors{
+struct Errors{ //Also expect to be moved to it's own header file
 
 };
 
@@ -27,7 +44,8 @@ public:
     virtual std::string GetFileName()         = 0;
     virtual bool HasNextFile()                = 0;
     virtual bool DoGetLine(std::string* line) = 0;
-    bool OpenNextFile();
+    virtual bool OpenNextFile()               = 0;
+    void RestorePoint(int position);
     bool GetLine(std::string* line);
     int GetLineNo();
 };
@@ -39,11 +57,19 @@ public:
 
 };
 
-class FileInputSource : InputSource {
+class FileInputSource : public InputSource {
 private:
     std::ifstream inputStream;
+    std::vector<std::string> fileNames;
 
 public:
+    FileInputSource(){};
+    ~FileInputSource();
+    void AddFile(const std::string& filePath);
+    bool OpenNextFile() override;
+    bool HasNextFile() override;
+
+
 
 
 };
@@ -54,7 +80,6 @@ private:
     Errors errorList;
 
 public:
-
     Lexor(std::string conSource, Errors conError){
         source = conSource;
         errorList = conError;
