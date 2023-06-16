b0c03dbd70f4341c834c8adb99eeaf869a00cf6a
Najir <irrperks@gmail.com>
2023-06-15 20:58:22 -0700

    Started working on class function definitions

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,3 +1,24 @@
 #include "lexer.h"
 #include <iostream>
 
+    Lexor(Tokens conToken, Errors conError){
+
+    };
+    void AddFile(const std::string& filePath){
+
+    };
+    bool OpenNextFile(){
+
+    };
+    bool HasNextFile(){
+
+    };
+    void RestorePoint(int position){
+
+    };
+    bool GetLine(std::string* line){
+
+    };
+    int GetLineNo(){
+        
+    };
